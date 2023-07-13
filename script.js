f35ce5c72dc44350ebd20a5ff40ee2faf98dc09a
MoinMN <moinnaik45@eng.rizvi.edu.in>
2023-07-13 12:30:05 +0530

    Version 2.0

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-let input_text = document.getElementById('first-text');
-let input_option_one = document.getElementById('temp1');
+let input_text_first = document.getElementById('first-text');
+let input_text_second = document.getElementById('second-text');
+let input_option_first = document.getElementById('temp1');
 let input_option_second = document.getElementById('temp2');
 
-input_text.addEventListener('input', convert);
-input_option_one.addEventListener('input', convert);
-input_option_second.addEventListener('input', convert);
+input_text_first.addEventListener('input', convertRight);
+input_text_second.addEventListener('input', convertLeft);
+input_option_first.addEventListener('input', convertRight);
+input_option_second.addEventListener('input', convertRight);
 
-function convert() {
+function convertRight() {
     let first_option = document.getElementById('temp1').value
-    let second_option = document.getElementById('temp2').value
-    
+    let second_option = document.getElementById('temp2').value    
     let first_text = document.getElementById('first-text').value
     let second_text = document.getElementById('second-text')
     
@@ -24,7 +25,6 @@ function convert() {
     }
     
     second_option = document.getElementById('temp2').value
-
 
     if (first_option == 'Degree Celsius' && second_option == 'Fahrenheit') 
         second_text.value = (Number(first_text) * 9/5) + 32;
@@ -42,4 +42,40 @@ function convert() {
         console.log('Else One')
 }
 
-convert();
+function convertLeft() {
+    let first_option = document.getElementById('temp1').value
+    let second_option = document.getElementById('temp2').value    
+    let first_text = document.getElementById('first-text')
+    let second_text = document.getElementById('second-text').value
+    
+    if (first_option == 'Kelvin' && second_option == 'Kelvin')  {
+        document.getElementById('temp2').value = "Degree Celsius";
+    }
+    if (first_option == 'Fahrenheit' && second_option == 'Fahrenheit'){
+        document.getElementById('temp2').value = "Degree Celsius";
+    }
+    if (first_option == 'Degree Celsius' && second_option == 'Degree Celsius'){
+        document.getElementById('temp2').value = "Fahrenheit";
+    }
+    
+    second_option = document.getElementById('temp2').value
+
+    if (first_option == 'Degree Celsius' && second_option == 'Fahrenheit') 
+        first_text.value = (Number(second_text) - 32) * 5/9;
+    else if (first_option == 'Degree Celsius' && second_option == 'Kelvin')
+        first_text.value = Number(second_text) - 273.15;
+    else if (first_option == 'Fahrenheit' && second_option == 'Degree Celsius')
+        first_text.value = (Number(second_text) * 9/5) + 32;
+    else if (first_option == 'Fahrenheit' && second_option == 'Kelvin')
+        first_text.value = (Number(second_text) - 273.15) * 9/5 + 32;
+    else if (first_option == 'Kelvin' && second_option == 'Degree Celsius')
+        first_text.value = Number(second_text) + 273.15;
+    else if (first_option == 'Kelvin' && second_option == 'Fahrenheit')
+        first_text.value = (Number(second_text) - 32) * 5/9 + 273.15;
+    else
+        console.log('Else One')
+}
+
+
+
+convertRight();
